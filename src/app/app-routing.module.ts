9178ae384d0c6f09cbf5f2806ee7fe0316378342
santironhacker <santi92.network@gmail.com>
2023-03-21 23:38:54 +0700

    [master] Created CharacterSheet component. Added routes and made it default component to display.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharacterSheetComponent } from './components/character-sheet/character-sheet/character-sheet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'character-sheet', component: CharacterSheetComponent },
+  { path: '',   redirectTo: '/character-sheet', pathMatch: 'full' },
+  { path: '**', component: CharacterSheetComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpClientModule } from '@angular/common/http';
+import { CharacterSheetComponent } from './components/character-sheet/character-sheet/character-sheet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharacterSheetComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
